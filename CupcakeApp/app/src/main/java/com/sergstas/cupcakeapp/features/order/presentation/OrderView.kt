4b3a4296ff5fef701eaaf86a75e0d274f43153f0
Sergey <sergstas0@gmail.com>
2020-12-21 14:54:13 +0500

    Attempts to use Moxy

--- CupcakeApp/app/src/main/java/com/sergstas/cupcakeapp/features/order/presentation/OrderView.kt
+++ CupcakeApp/app/src/main/java/com/sergstas/cupcakeapp/features/order/presentation/OrderView.kt
@@ -1,6 +1,12 @@
 package com.sergstas.cupcakeapp.features.order.presentation
 
+import moxy.MvpView
+import moxy.viewstate.strategy.SkipStrategy
+import moxy.viewstate.strategy.StateStrategyType
 
-interface OrderView {
+
+interface OrderView: MvpView {
+
+    @StateStrategyType(SkipStrategy::class)
     fun showErrorToast(error: ValidationError)
 }
